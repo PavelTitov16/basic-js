592c88407fd75cebf311ed1767c2ac7e67880bac
PavelTitov16 <pashoktitov16@gmail.com>
2022-04-14 00:33:49 +0300

    feat: add carbon dating solution

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -1,4 +1,6 @@
-const { NotImplementedError } = require('../extensions/index.js');
+const {
+  NotImplementedError
+} = require('../extensions/index.js');
 
 const MODERN_ACTIVITY = 15;
 const HALF_LIFE_PERIOD = 5730;
@@ -17,10 +19,18 @@ const HALF_LIFE_PERIOD = 5730;
  * dateSample('WOOT!') => false
  *
  */
-function dateSample(/* sampleActivity */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function dateSample(sampleActivity) {
+  const activity = 15;
+  const Th = 5730;
+  const k = 0.693;
+
+  if (typeof sampleActivity === "string" && sampleActivity / 1 > 0 && sampleActivity < activity) {
+    return Math.ceil(Math.log(activity / sampleActivity) / (k / Th) );
+  } 
+  return false;
 }
+
+
 
 module.exports = {
   dateSample
